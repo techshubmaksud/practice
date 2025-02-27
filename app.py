14ae66093c952e4174b5070dc29f99eb61a30027
Maksud Vahora <techstrainner@gmail.com>
2025-02-28 00:26:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 def addition(num1, num2):
     return num1 + num2
-def subtraction(num1, num2):
-    return num1 - num2
-<<<<<<< HEAD
+def subtraction(num1, num2, num3):
+    return num1 - num2 - num3
 
-=======
 def multiple(num1, num2):
     return num1 * num2
 def division(num1, num2):
     return num1 / num2
->>>>>>> developera
