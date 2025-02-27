6902297114d9aed14008a7f2c19ddd6160e4b591
Maksud Vhora <techstrainner@gmail.com>
2025-02-28 00:19:24 +0530

    developer A commit-1

--- app.py
+++ app.py
@@ -2,3 +2,7 @@ def addition(num1, num2):
     return num1 + num2
 def subtraction(num1, num2):
     return num1 - num2
+def multiple(num1, num2):
+    return num1 * num2
+def division(num1, num2):
+    return num1 / num2
